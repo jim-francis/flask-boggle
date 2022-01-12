153a5691669ad08ec1a19d59135112f614f46405
Jimmy Francis <jimmyfrancis@protonmail.com>
2022-01-11 20:32:24 -0500

    Setup Flask

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+from flask import Flask, request, render_template, redirect, flash, session
 from boggle import Boggle
 
+app = Flask(__name__)
+
 boggle_game = Boggle()
+
+@app.route('/')
+def show_homepage():
+    return render_template("index.html")
