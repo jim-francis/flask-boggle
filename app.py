38ae46773f45c510d6fc6b59a789ba7bf7838937
Jimmy Francis <jimmyfrancis@protonmail.com>
2022-01-17 20:50:40 -0500

    Get an ajax response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash, session
+from flask import Flask, request, render_template, redirect, flash, session, jsonify
 from boggle import Boggle
 
 app = Flask(__name__)
@@ -12,3 +12,11 @@ def show_homepage():
     board = boggle_game.make_board()
     session['board'] = board
     return render_template("index.html", board=board)
+
+@app.route("/check-word")
+def check_word():
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board,word)
+    
+    return jsonify({"result": response})
--- static/app.js
+++ static/app.js
@@ -1,3 +1,12 @@
-$('#submitForm').submit(function(e){
+
+$('#submit-form').submit(async function(e){
     e.preventDefault();
+    let word = getWord()
+    const res = await axios.get("/check-word", { params: { word: word }})
+    console.log(res)
 });
+
+function getWord(){
+    const $word = $(".word").val()
+    return $word
+}
