99dcd4c9e35dcaaca3054850273c60bbcc11f559
Jimmy Francis <jimmyfrancis@protonmail.com>
2022-01-17 21:47:22 -0500

    Impliment check-word

--- static/app.js
+++ static/app.js
@@ -2,11 +2,32 @@
 $('#submit-form').submit(async function(e){
     e.preventDefault();
     let word = getWord()
-    const res = await axios.get("/check-word", { params: { word: word }})
-    console.log(res)
+    // let res = await axios.get("/check-word", { params: { word: word }})
+    let res = await getResponse(word)
+    checkWord(res)
+    $(".word").val("").focus()
 });
 
 function getWord(){
     const $word = $(".word").val()
     return $word
+}
+
+async function getResponse(word){
+    let res = await axios.get("/check-word", { params: { word: word }})
+    return res.data.result
+}
+
+function checkWord(res){
+    if (res === "not-word"){
+        postMessage("Not a word", "error")
+    } else if (res === "not-on-board"){
+        postMessage("Invalid word", "error")
+    } else {
+        postMessage("Score!", "success")
+    }
+}
+
+function postMessage(message, style){
+    $(".messages").text(message).removeClass().addClass(`message ${style}`)
 }
